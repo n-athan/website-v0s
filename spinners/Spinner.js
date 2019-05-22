092c93a1c6fb512db0b6a1691802bca0b209fee0
Nathan Vos <nathan.vos@ogd.nl>
2019-05-22 16:21:47 +0200

    Added spinner page

---
+++ spinners/Spinner.js
@@ -0,0 +1,91 @@
+class Spinner {
+	
+  constructor(color,arms,size) {
+		this.offset = 0;
+		this.x = random(width);
+		this.y = random(height);
+		this.max = random(20,10);
+    this.speed = this.max;
+		this.slow = 0.01;
+		this.color = color;
+		this.arms = arms;
+    this.mirror_angle = 360/this.arms/2;
+    this.size = size;
+    this.radius = (PI*this.size)/(this.arms*2);
+    this.moving = false;
+    this.shape = this.createShape();
+	}  
+	
+	update() {
+      if(this.speed > 0) {
+        this.speed -= this.slow;
+		this.offset += this.speed;	
+      }
+      if(this.moving == true) {
+        this.x = mouseX;
+        this.y = mouseY;
+      }
+	}
+  
+  createShape() { //creates the shape of the spinner and saves it as an image object.
+    let shape = createGraphics(this.size*4,this.size*4);
+    shape.colorMode(HSB,360,100,100,1);	
+    shape.background(0,0,0,0); //transparant
+    shape.fill(this.color);
+    shape.beginShape();
+    let counter = 0;
+    //start of curve (first vertex is startpoint)
+    shape.curveVertex(
+      shape.width/2+cos(360)*this.size+cos(90+360)*this.radius,
+      shape.height/2+sin(360)*this.size+sin(90+360)*this.radius);
+    //curve vertices
+    for (let j = 360; j > 0; j -= this.mirror_angle) {      
+      let cx = shape.width/2+cos(j)*this.size;
+      let cy = shape.height/2+sin(j)*this.size;
+      let s = this.radius;
+      if (counter%2 == 0){
+        for (let k = 90; k >= -90; k -= 5) {
+          shape.curveVertex(cx+cos(k+j)*s,cy+sin(k+j)*s);         }
+      } else {
+        for (let k = 90; k <= 270; k += 5) {
+          shape.curveVertex(cx+cos(k+j)*s,cy+sin(k+j)*s);   
+        }
+      }
+      counter++;
+    }
+    //end of curve
+    shape.curveVertex(
+      shape.width/2+cos(360)*this.size+cos(90+360)*this.radius,
+      shape.height/2+sin(360)*this.size+sin(90+360)*this.radius);
+    shape.endShape(CLOSE);
+    //center disk.
+    shape.fill(0,0,0);
+    let rCenter = (this.size-this.radius);
+    shape.ellipse(shape.width/2,shape.height/2,rCenter,rCenter);
+    
+    return shape;
+  }
+  
+  show() {
+    push();
+    translate(this.x,this.y);
+    rotate(this.offset);
+    image(this.shape,-this.shape.width/2,-this.shape.height/2);
+    pop();    
+  }
+      
+  move(x,y) {
+    let d = dist(this.x,this.y,x,y);
+    if (d < (this.size-this.radius)/2) {
+      this.moving = true;
+    }
+  }
+  
+  start(x,y) {
+    let d = dist(this.x,this.y,x,y);
+    if (d < (this.size-this.radius)/2) {
+      this.speed = this.max;
+    }
+  }
+
+}
---
+++ spinners/sketch.js
@@ -0,0 +1,90 @@
+let spinners = [];
+let sliders;
+let block = document.getElementById("colorBlock");
+
+function setup() {
+  createCanvas(innerWidth,innerHeight);  
+  colorMode(HSB,360,100,100,1);	
+  angleMode(DEGREES);
+  //interface setup
+  sliders = selectAll('.slider');
+	showInterface();
+
+  // create first spinner
+    newSpinner();
+
+  //smallest spinner on top
+  spinners.sort((a,b) => b.size - a.size);
+}
+
+function draw() {  
+  //dark blue background with slight blur for movement.
+  background(249, 30, 15, 0.7);
+
+  // drawing spinners
+  for (let i = 0; i < spinners.length; i++){
+  spinners[i].show(); 
+  }
+  
+  // rotation of spinners
+  for (let i = 0; i < spinners.length; i++){
+  spinners[i].update(); 
+  }
+
+  let hue = sliders[0].value();
+	let sat = sliders[1].value();
+	let brg = sliders[2].value();
+  block.style = 'background-color: hsl('+hue+', '+sat+'%, '+brg+'%);';
+}
+
+//start moving spinner
+function mousePressed() {  
+  for (let i = 0; i < spinners.length; i++){
+    spinners[i].move(mouseX,mouseY); 
+  }
+}
+
+//stop moving spinner and give it max speed. 
+function mouseReleased() {  
+  for (let i = 0; i < spinners.length; i++){
+    spinners[i].start(mouseX,mouseY);
+    spinners[i].moving = false; 
+  }
+}
+
+//Create a new spinner with user input.
+function newSpinner() {
+  let hue = sliders[0].value();
+	let sat = sliders[1].value();
+	let brg = sliders[2].value()*2;
+  let clr = color(hue,sat,brg);
+  let size = select('#size').value();
+  let arms = select('#arms').value();
+
+  spinner = new Spinner(clr,arms,size);
+  spinners.push(spinner);
+}
+
+//remove spinner on doubleclick- not compatible on most browsers yet.
+window.addEventListener('dblclick', doubleClicked());
+function doubleClicked() {
+  for (let i = 0; i < spinners.length; i++){
+    let sp = spinners[i];
+    let d = dist(sp.x,sp.y,mouseX,mouseY);
+    if (d < (sp.size-sp.radius)/2) {
+      spinners.splice(i,1);
+    }
+  } 
+} 
+
+
+//Interface funtionality
+function showInterface() {
+	let i = select('#settingsInterface');
+	i.style('top', '2em');
+}
+
+function hideInterface() {
+	let i = select('#settingsInterface');
+	i.style('top', '-450px' );
+}
